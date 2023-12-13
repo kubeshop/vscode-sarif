0611dfc0bba247ff1656b16920a20c3653766ec7
Krzysztof Krztoń <krzysztof@kubeshop.io>
2023-12-13 10:49:54 +0100

    chore: rename hint to remediation

--- src/panel/details.tsx
+++ src/panel/details.tsx
@@ -72,7 +72,7 @@ interface DetailsProps { result: Result, resultsFixed: string[], height: IObserv
                             <span>Rule Id</span>			{helpUri ? <a href={helpUri} target="_blank" rel="noopener noreferrer">{result.ruleId}</a> : <span>{result.ruleId}</span>}
                             <span>Rule Name</span>			<span>{result._rule?.name ?? '—'}</span>
                             <span>Rule Description</span>	<span>{renderRuleDesc(result)}</span>
-                            <span><strong>Hint</strong></span>				<span>{result._rule?.help?.text ?? '—'}</span>
+                            <span>Remediation</span>				<span>{result._rule?.help?.text ?? '—'}</span>
                             <span>Level</span>				<span>{result.level}</span>
                             <span>Locations</span>			<span className="svDetailsGridLocations">
                                                                 {result.locations?.map((loc, i) => {
